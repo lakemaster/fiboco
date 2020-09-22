e92e1ceb726b0b17cce07f642a06f6609cf023a7
lakemaster <lakelord@jojobi.de>
2020-09-22 13:44:07 +0200

    adding to list

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ cost_map = {
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    cid: int = request.values.get("id")
+    cid: int = request.values.get("cid")
     desc = request.values.get("desc")
     amount = request.values.get("amount")
     payer = request.values.get("payer")
@@ -20,6 +20,39 @@ def hello():
     if cdate is None:
         cdate = date.today()
 
-    print("Received: Cid=" + str(cid) + " - " + str(desc))
+    if empty(cid) and not empty(desc) and not empty(amount) and not empty(payer):
+        add(desc, amount, payer, cdate)
+
+    if not empty(cid) and not empty(desc) and not empty(amount) and not empty(payer):
+        update(cid, desc, amount, payer, cdate)
+
+    print("Received: cid=<" + str(cid)
+          + "> desc=<" + str(desc)
+          + "> amount=<" + str(amount)
+          + "> payer=<" + str(payer)
+          + "> cdate=<" + str(cdate))
+
     return render_template("fiboco.html",
                            cid=cid, desc=desc, amount=amount, payer=payer, cdate=cdate, cost_map=cost_map)
+
+def empty(x):
+    return x is None or not x
+
+def add(desc, amount, payer, cdate):
+    cid = max(cost_map.keys()) + 1
+    print("Add: cid=<" + str(cid)
+          + "> desc=<" + str(desc)
+          + "> amount=<" + str(amount)
+          + "> payer=<" + str(payer)
+          + "> cdate=<" + str(cdate))
+
+    cost_map[cid] = [desc, amount, payer, cdate]
+
+def update(cid, desc, amount, payer, cdate):
+    print("Update: cid=<" + str(cid)
+          + "> desc=<" + str(desc)
+          + "> amount=<" + str(amount)
+          + "> payer=<" + str(payer)
+          + "> cdate=<" + str(cdate))
+
+    cost_map[cid] = [desc, amount, payer, cdate]
