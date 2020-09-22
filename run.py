5efdec0e7fa27baba5a9359f4573cc52c107c9e3
lakemaster <lakelord@jojobi.de>
2020-09-22 12:51:49 +0200

    switch to int and date types in list

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 # coding=utf-8
 from flask import Flask, render_template, request
+from datetime import datetime, date
 app = Flask(__name__)
 
 cost_map = {
-    1: ["Rolle für Trailer", "20 EUR", "Dierk", "08.09.2020"],
-    2: ["Aufkleber Jodijo", "6,99 EUR", "Jochen", "17.09.2020"]
+    1: ["Rolle für Trailer", 20.00, "Dierk", date(2020, 9, 8)],
+    2: ["Aufkleber Jodijo", 6.99, "Jochen", date(2020, 9, 17)]
 }
 
 
@@ -14,6 +15,11 @@ def hello():
     desc = request.values.get("desc")
     amount = request.values.get("amount")
     payer = request.values.get("payer")
-    date = request.values.get("date")
+    cdate = request.values.get("cdate")
+
+    if cdate is None:
+        cdate = date.today()
+
     print("Received: Cid=" + str(cid) + " - " + str(desc))
-    return render_template("fiboco.html", cid=cid, desc=desc, amount=amount, payer=payer, date=date, cost_map=cost_map)
+    return render_template("fiboco.html",
+                           cid=cid, desc=desc, amount=amount, payer=payer, cdate=cdate, cost_map=cost_map)
