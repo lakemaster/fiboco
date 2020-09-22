d0eb9f31e870294ca6f449219f2b97965d5a6e16
lakemaster <lakelord@jojobi.de>
2020-09-22 11:46:02 +0200

    add form and list to html

--- run.py
+++ run.py
@@ -2,8 +2,13 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=['GET', 'POST'])
 def hello():
-    return render_template("start.html")
-
-
+    cid: int = request.values.get("id")
+    desc = request.values.get("desc")
+    amount = request.values.get("amount")
+    payer = request.values.get("payer")
+    date = request.values.get("date")
+    print("Received: Cid=" + str(cid) + " - " + str(desc))
+    return render_template("fiboco.html", cid=cid, desc=desc, amount=amount, payer=payer, date=date)
