0f4890cc3305d6d5514e73fe1f2c0bb18412c938
lakemaster <lakelord@jojobi.de>
2020-09-22 12:11:25 +0200

    fill list with costs

--- run.py
+++ run.py
@@ -2,6 +2,11 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+cost_map = {
+    1: ["Rolle für Trailer", "20 EUR", "Dierk", "08.09.2020"],
+    2: ["Aufkleber Jodijo", "6,99 EUR", "Jochen", "17.09.2020"]
+}
+
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
@@ -11,4 +16,4 @@ def hello():
     payer = request.values.get("payer")
     date = request.values.get("date")
     print("Received: Cid=" + str(cid) + " - " + str(desc))
-    return render_template("fiboco.html", cid=cid, desc=desc, amount=amount, payer=payer, date=date)
+    return render_template("fiboco.html", cid=cid, desc=desc, amount=amount, payer=payer, date=date, cost_map=cost_map)
