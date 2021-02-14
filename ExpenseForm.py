3944557501f0826ddf5e997cf633caeb08ba73f1
Jochen <lakelord@jojobi.de>
2021-02-14 09:04:46 +0100

    add payer combobox

--- ExpenseForm.py
+++ ExpenseForm.py
@@ -1,13 +1,15 @@
 from datetime import date
-from wtforms import Form, StringField, DateField, DecimalField, HiddenField, validators
+from wtforms import Form, StringField, DateField, DecimalField, HiddenField, SelectField, validators
 
 
 class ExpenseForm(Form):
     class Meta:
         locales = ['de']
 
+    PAYER_CHOICES = [('Dierk', 'Dierk'), ('Jochen', 'Jochen')]
+
     id = HiddenField('Id')
     description = StringField('Beschreibung', [validators.DataRequired(), validators.Length(min=3, max=255)], default='')
     amount = DecimalField('Betrag', [validators.DataRequired()], use_locale=True)
-    payer = StringField('Zahler', [validators.DataRequired(), validators.Length(min=3, max=32)], default='')
+    payer = SelectField(label='Zahler', choices=PAYER_CHOICES, default='D')
     date = DateField('Datum', [validators.DataRequired()], format='%d.%m.%Y', default=date.today())
